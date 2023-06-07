cb51c5a94ab8eaedc33ee0d7b92854d96e997cbe
AllanDevincolagit <profissional.allandevincola@gmail.com>
2023-06-07 12:05:21 -0300

    cadlog

---
+++ src/cadastro.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Cadastro = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleCadastro = (e) => {
+    e.preventDefault();
+    // Lógica de cadastro aqui
+  };
+
+  return (
+    <div>
+      <h2>Cadastro</h2>
+      <form onSubmit={handleCadastro}>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Cadastro;
